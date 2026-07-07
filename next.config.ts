a2221e6eece7dcbd95929018aa859a5d1d61bac7
MircoFernando <mircofdo@gmail.com>
2026-07-08 01:50:35 +0530

    fix: config fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/DevDash26" : undefined,
   images: {
     unoptimized: true,
   },
