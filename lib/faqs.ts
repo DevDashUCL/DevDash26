2d4e42b53a94d53ef8bd8975e9388b3e4c508557
MokshaSan <sandavirage@gmail.com>
2026-07-08 13:18:25 +0530

    change the q/a s in faq

--- lib/faqs.ts
+++ lib/faqs.ts
@@ -4,33 +4,28 @@ export interface FAQ {
 }
 export const FAQS = [
   {
-    question: "What is DevDash 2026?",
+    question: "What is DevDash'26?",
     answer:
-      "DevDash 2026 is a university hackathon organized by the University of Colombo School of Computing (UCSC). It is a platform for students to showcase their coding skills, collaborate with peers, and develop innovative solutions to real-world problems.",
+      "DevDash'26 is a university hackathon organized by the Universal College Lanka. It is a platform for students to showcase their coding skills, collaborate with peers, and develop innovative solutions to real-world problems.",
   },
   {
-    question: "Who can participate in DevDash 2026?",
+    question: "Who can participate in DevDash'26?",
     answer:
-      "DevDash 2026 is open to all university students who are interested in coding, technology, and innovation. Participants can form teams and work on projects during the hackathon.",
+      "DevDash 26 is open to all currently enrolled undergraduate students at UCL. Students from any year of study or degree programme are welcome to participate, reguardless of their previous hackathon experience.",
   },
   {
-    question: "How do I register for DevDash 2026?",
+    question: "How do I register for DevDash'26?",
     answer:
       `You can register for DevDash 2026 by filling out the registration form available at the registration page. Make sure to provide accurate information and submit the form before the registration deadline.`,
   },
   {
-    question: "What are the prizes for DevDash 2026?",
-    answer:
-      "DevDash 2026 offers various prizes, including cash prizes and certificates for the top teams. The specific details of the prizes will be announced closer to the event date.",
-  },
-  {
     question: "Can I participate as an individual or do I need a team?",
     answer:
-      "Participants can either form teams or participate individually. However, it is recommended to form a team to collaborate and leverage diverse skills during the hackathon.",
+      "DevDash'26 is a team based hackathon. Each team must have minimum of 2 members and maximum of 5 members. All participents must register as a team before the registration deadline",
   },
   {
     question: "What should I bring to the hackathon?",
     answer:
-      "Participants should bring their own laptops, chargers, and any other necessary equipment. It is also advisable to bring a notebook and pen for taking notes.",
+      "Participants should bring their own laptops and any other necessary equipment.",
   },
 ];
