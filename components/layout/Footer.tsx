8b984b709f0ded1d1cde7ee7a440b2e28f4b2ce2
MokshaSan <sandavirage@gmail.com>
2026-07-09 14:52:33 +0530

    updated register section

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,12 +1,43 @@
 "use client";
 
 import React from "react";
+import Image from "next/image";
+import "../ui/Footer.css";
 
 export default function Footer() {
   return (
-    <footer className="relative w-full py-10 bg-[#0A0A0A] border-t border-[#2A2A2A] flex items-center justify-center">
-      <div className="text-center text-sm text-[#9A9A9A]">
-        <p>&copy; 2026 DevDash. All rights reserved.</p>
+    <footer className="footer-section border-t border-[#2A2A2A] bg-[#0A0A0A]">
+      <div className="footer-inner max-w-7xl">
+        <div className="footer-logo-left">
+          <Image
+            src="/logos/csc-ucl-transparent.png"
+            alt="CSC UCL"
+            width={120}
+            height={40}
+            className="h-10 w-auto object-contain"
+          />
+        </div>
+
+        <div className="footer-center">
+          <p>&copy; DevDash&apos;26</p>
+        </div>
+
+        <div className="footer-logos-right">
+          <Image
+            src="/ucl.png"
+            alt="UCL"
+            width={80}
+            height={40}
+            className="h-8 w-auto object-contain sm:h-10"
+          />
+          <Image
+            src="/logos/uclan-stacked-transparent.png"
+            alt="UCLan"
+            width={100}
+            height={40}
+            className="h-8 w-auto object-contain sm:h-10"
+          />
+        </div>
       </div>
     </footer>
   );
